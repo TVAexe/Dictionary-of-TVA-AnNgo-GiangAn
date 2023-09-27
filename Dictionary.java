27d00065c11c25fd1353e249623b9910df2f1d1c
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2023-09-27 08:15:15 +0700

    đọc file

--- Dictionary.java
+++ Dictionary.java
@@ -4,34 +4,52 @@ import java.util.*;
 
 public class Dictionary {
     private List<Word> dictionary = new ArrayList<Word>();
-    public void addWord(String eng,String type,List<String> vie)
-    {
-        dictionary.add(new Word(eng,vie));
+
+    public void addWord(String word, String pronoun, List<String> meaning) {
+        dictionary.add(new Word(word, pronoun, meaning));
     }
-    // Lấy giữ liệu từ đường dẫn đến file 's' , chuẩn hóa gán vào một đối tượng Word, rồi add vào dictionary
-    public void addWordFromFile(String s)
-    {
-        try(BufferedReader reader = new BufferedReader(new FileReader(s))) {
-            String line;
-            String wordTaget="";
-            List<String> wordExplain = new ArrayList<>();
-            while((line = reader.readLine()) != null) {
-                if(line.startsWith("@")) {
-                    wordTaget = line.substring(1).trim();
+
+    // Lấy giữ liệu từ đường dẫn đến file 's' , chuẩn hóa gán vào một đối tượng
+    // Word, rồi add vào dictionary
+    public void addWordFromFile(String s) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(s))) {
+            String s0;
+            while ((s0 = reader.readLine()) != null) {
+                String w = s0.trim();
+                String pr = s0.trim();
+                List<String> mean = new ArrayList<>();
+                String line;
+
+                while ((line = reader.readLine()) != null && !line.trim().isEmpty()) {
+                    line = line.trim(); // Loại bỏ khoảng trắng ở đầu và cuối dòng
+                    if (line.startsWith("- ")) {
+                        line = line.substring(2); // Loại bỏ dấu "- " ở đầu
+                    }
+                    mean.add(line);
                 }
-                else if(line.startsWith("-")) {
-                    wordExplain.add(line.substring(1).trim());
-                }
-                else if(line.isEmpty() && wordTaget != null && !wordExplain.isEmpty())
-                {
-                    Word word = new Word(wordTaget ,wordExplain);
-                    dictionary.add(word);
-                    wordTaget = "";
-                    wordExplain.clear();
-                }
+
+                addWord(w, pr, mean);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    void printTest() {
+        for (Word i : dictionary) {
+            System.out.println(i.getWordTarget());
+            System.out.println(i.getWordPronunciation());
+            List<String> test = i.getWordExplain();
+            for (String i0 : test) {
+                System.out.println(i0);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Dictionary entry = new Dictionary();
+        entry.addWordFromFile("Vocabulary.txt");
+        entry.printTest();
+
     }
 }
