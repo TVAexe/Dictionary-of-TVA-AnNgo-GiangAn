622c10f152d7ba21b8b8f67342ead58471fe5ee7
AnNgoexe <quocan2004tp@gmail.com>
2023-10-10 00:49:03 +0700

    Make commandline version

--- src/CommandLine/DictionaryManagement.java
+++ src/CommandLine/DictionaryManagement.java
@@ -1,9 +1,27 @@
-import java.util.*;
+// /**
+//      * Read data from a file and add words to the dictionary.
+//      * @param s The path to the file.
+//      */
+//     public void addWordFromFile(String s) {
+//         try (BufferedReader reader = new BufferedReader(new FileReader(s))) {
+//             String s0;
+//             while ((s0 = reader.readLine()) != null) {
+//                 String w = s0.trim();
+//                 String pr = reader.readLine().trim();
+//                 List<String> mean = new ArrayList<>();
+//                 String line;
 
-public class DictionaryManagement {
-   private Dictionary dc = new Dictionary();
-   
+//                 while ((line = reader.readLine()) != null && !line.trim().isEmpty()) {
+//                     line = line.trim(); // Remove whitespace at the beginning and end of the line
+//                     if (line.startsWith("- ")) {
+//                         line = line.substring(2); // Remove "- " at the beginning
+//                     }
+//                     mean.add(line);
+//                 }
 
-    
-
-}
+//                 addWord(w, pr, mean);
+//             }
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
+//     }
