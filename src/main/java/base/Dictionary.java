768ba9fc48db3a3ab65cb8be5f5c066a9c5c5f20
DAOGIANGAN <125682086+DAOGIANGAN@users.noreply.github.com>
2023-11-26 12:38:16 +0700

    demo

--- src/main/java/base/Dictionary.java
+++ src/main/java/base/Dictionary.java
@@ -1,6 +1,5 @@
 package base;
 
-
 import java.util.Map;
 import java.util.TreeMap;
 import java.io.BufferedReader;
--- src/main/java/com/example/dictionaryoftvaanngogiangan/HelloApplication.java
+++ src/main/java/com/example/dictionaryoftvaanngogiangan/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.example.dictionaryoftvaanngogiangan;
 
+import base.Dictionary;
+import base.MyDictionary;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +12,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Dictionary.getMyDictionary().insertFromFile();
+        MyDictionary.getMyDictionary().insertFromFile();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
