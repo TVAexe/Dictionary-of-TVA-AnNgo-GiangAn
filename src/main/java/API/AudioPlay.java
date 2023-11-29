c24fdcb6f494948a64d1375d5e98944ae02fd3d4
DAOGIANGAN <125682086+DAOGIANGAN@users.noreply.github.com>
2023-11-29 15:28:12 +0700

    update API

--- src/main/java/API/AudioPlay.java
+++ src/main/java/API/AudioPlay.java
@@ -11,10 +11,10 @@ import java.net.http.HttpResponse;
 
 public class AudioPlay {
 
-    public static void TTS(String text) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
+    public static void TTS(String text, String lang) throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException {
         String encodedText = URLEncoder.encode(text, "UTF-8");
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://voicerss-text-to-speech.p.rapidapi.com/?key=a1ebb15a22fa42ca8ad9b87523d3648e&src="+encodedText+"&hl=en-us&r=0&c=WAV&f=22khz_8bit_mono"))
+                .uri(URI.create("https://voicerss-text-to-speech.p.rapidapi.com/?key=a1ebb15a22fa42ca8ad9b87523d3648e&src="+encodedText+"&hl="+ lang +"&r=0&c=WAV&f=22khz_8bit_mono"))
                 .header("X-RapidAPI-Key", "76bc97a9d3mshee7d7c0825c7778p17f65ejsn6638c8856d73")
                 .header("X-RapidAPI-Host", "voicerss-text-to-speech.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
